6ae2f3ac01b52494fab6fa99465f120c859c0018
Mazcho <putrantonicholaus@gmail.com>
2023-10-15 23:50:21 +0700

    update

--- Minggu4 Vector Space/Projek MInggu 4/main.py
+++ Minggu4 Vector Space/Projek MInggu 4/main.py
@@ -168,32 +168,6 @@ with open('style.css')as f:
 
 #==== End of loading css =========
 
-import base64
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('image\logo-removebg-preview (2).png')
-
-
-
 
 #=======Layout=============
 
@@ -218,8 +192,7 @@ with st.sidebar:
 if menuapp=="Home":
     with st.container():
         st.header("Ingin Nostalgia dengan film lama? atau ingin nonton film yang update? Udah buruan segera cari di Dunia Film!")
-    st.markdown('\n')
-    judul_film = st.text_input("")
+    judul_film = st.text_input("Masukan Judulmu disini")
     if judul_film != "":
         user_input_title(judul_film)
     st.write("Top 5 film berdasarkan vote")
